dd4ddeccf6ce2d05a73e37040e45cce458d9a0b1
Graham Howden <graham_howden1@yahoo.co.uk>
2014-11-04 11:25:29 +0000

    fix: need to defer returning of the list of fetched videos

--- src/lib/video-fetcher.js
+++ src/lib/video-fetcher.js
@@ -1,15 +1,21 @@
+var q = require('q');
+
 module.exports = function VideoFetcher(robot) {
 
     /**
      * Fetches latest video list for the given user
      * @param {string} username - The account to check
-     * @returns {{id: string, link: string}[]} list of id's and links for each video
+     * @returns {promise} promise that resolves to {id: string, link: string}[], a list of id's and links for each video
      */
     var fetchVideosForUser = function(username) {
+        var result = q.defer();
         robot.http('https://gdata.youtube.com/feeds/api/users/' + username + '/uploads?alt=json')
             .header('Accept', 'application/json')
             .get(function (err, res, body) {
-                if (err) return;
+                if (err) {
+                    result.reject(err);
+                    return;
+                }
                 var videos = [];
                 JSON.parse(body).feed.entry.forEach(function(element) {
                     videos.push({
@@ -17,8 +23,9 @@ module.exports = function VideoFetcher(robot) {
                         link: element.link[0]
                     });
                 });
-                return videos;
+                result.resolve(videos);
             });
+        return result.promise;
     };
 
     return {
